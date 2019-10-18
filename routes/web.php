83ce4b1eb9a2f722afca5752f7752615acdd836e
Yuhei Tokimatsu <y.tokimatsu@sus-c.co.jp>
2019-10-18 19:58:31 +0900

    勤怠管理画面・ヘッダーの作成
    レイアウトを作成したのち、header.blade.php、attendanceList.blade.phpを作成した。リスト内やテーブルの値は一旦手打ち。

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/attendanceList', function () {
+//     return view('attendanceList');
+// });
+Route::get('/attendanceList','TopController@index');
